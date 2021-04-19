edee35ac0bfca37bb07d063bdb6ce5ad30e8dcc1
Mads Andersen <madssighlund@live.dk>
2021-04-19 09:21:29 +0200

    Persistence functionality and cleaner code

--- src/main/java/persistence/KitchenPersistence.java
+++ src/main/java/persistence/KitchenPersistence.java
@@ -1,16 +1,7 @@
 package persistence;
 
-import model.CalculationModel;
-import model.FoodItemModel;
-import model.IngredientModel;
 import model.KitchenModel;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-
-import javax.persistence.*;
-
 
 public class KitchenPersistence {
 
@@ -19,14 +10,13 @@ public class KitchenPersistence {
     public static void create(){
         //TODO
         KitchenModel madEksempel = new KitchenModel();
-        madEksempel.setName("Test");
+        madEksempel.setName("Test123");
 
         Session s = SetupPersistence.getSf().openSession();
         s.beginTransaction();
         s.getTransaction().commit();
         s.save(madEksempel);
         s.close();
-
     }
 
     public static void update(){
--- src/main/java/persistence/SetupPersistence.java
+++ src/main/java/persistence/SetupPersistence.java
@@ -1,35 +1,12 @@
 package persistence;
 
 import model.*;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class SetupPersistence {
-/*
-    public static SessionFactory sessionFactory;
 
-    public static void setup(){
-        //Laver forbindelse til databasen med CalculationModel som den class den arbejder med
-        Configuration configuration = new Configuration();
-        configuration.configure("hibernate.cfg.xml");
-        configuration.addAnnotatedClass(CalculationModel.class)
-                .addAnnotatedClass(ConcitoItemModel.class)
-                .addAnnotatedClass(FoodDescriptorModel.class)
-                .addAnnotatedClass(FoodItemModel.class)
-                .addAnnotatedClass(IngredientModel.class).
-                addAnnotatedClass(KitchenModel.class);
-
-        StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-        SessionFactory sf = configuration.buildSessionFactory(ssrb.build());
-
-        Session s = sf.openSession();
-        s.beginTransaction();
-        s.getTransaction().commit();
-        s.close();
-    }
-*/
     private static final SessionFactory sf = setSessionFactory();
 
     private static SessionFactory setSessionFactory() {
@@ -56,24 +33,6 @@ public class SetupPersistence {
 
     public static void exit(){
         getSf().close();
-    }
-
-
-
-        //SetupPersistence.sessionFactory = sessionFactory;
-
-
-
-    public static void create(){
-        KitchenModel madEksempel = new KitchenModel();
-        madEksempel.setName("Test");
-        madEksempel.setId(1);
-
-        Session s = sf.openSession();
-        //session.beginTransaction();
-        //session.getTransaction().commit();
-        //sf.close();
-
     }
 
 }
