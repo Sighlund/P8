b640d88c1692abed76f69534fe9ff4e6dc2dfe26
BigGamerFan <72791021+BigGamerFan@users.noreply.github.com>
2021-04-27 15:41:58 +0200

    Memory optimization of FXMLload All switchScene in the controllers now only references the already loaded .fxml files found in the init method in App.java. + dokumentation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,39 +16,37 @@ import java.io.IOException;
  */
 public class App extends Application {
 
+    //roots being declared, which will later hold .fxml files.
     private static Parent calculationPageParent;
     private static Parent dataInsertionPageParent;
     private static Parent frontPageParent;
     private static Parent historyPageParent;
     private static Parent registerNewProductPageParent;
 
+    //References to the current active stage and scene are held here.
     private static Stage stage;
     private static Scene scene;
-
-
 
     public static Parent getDataInsertionPageParent() {
         return dataInsertionPageParent;
     }
-
     public static Parent getCalculationPageParent() {
         return calculationPageParent;
     }
-
     public static Parent getFrontPageParent() {
         return frontPageParent;
     }
-
     public static Parent getHistoryPageParent() {
         return historyPageParent;
     }
-
     public static Parent getRegisterNewProductPageParent() {
         return registerNewProductPageParent;
     }
 
+
     @Override
     public void init() throws IOException{
+        //We fill the roots with respective .fxml files from harddrive.
         calculationPageParent = FXMLLoader.load(getClass().getResource("calculationPage.fxml"));
         dataInsertionPageParent = FXMLLoader.load(getClass().getResource("dataInsertionPage.fxml"));
         frontPageParent = FXMLLoader.load(getClass().getResource("frontPage.fxml"));
@@ -56,32 +54,33 @@ public class App extends Application {
         registerNewProductPageParent = FXMLLoader.load(getClass().getResource("registerNewProductPage.fxml"));
     }
 
+    /**
+     * Starts the application. Sets stage, scene and root to hold contents.
+     * @param stage
+     * @throws IOException
+     */
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("frontPage"));
+    public void start(Stage stage){
+        scene = new Scene(frontPageParent);
         stage.setTitle("👨‍🍳👨‍🍳👨‍🍳Den store Bagedyst sæsonafslutning😎😎😎");
         Image icon = new Image("https://github.com/Sighlund/P8/blob/main/src/main/resources/img/Logo.PNG?raw=true");
         stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
+        //Updates the current active stage to equal the one filled in this method.
         this.stage = stage;
     }
 
+    public static void main(String[] args){
+        launch();
+    }
+
+    /**
+     * Auxiliary method to switch scenes from controllers. The controllers pass respective root.
+     * @param page the Parent root to be switched to
+     */
     public static void switchScene(Parent page){
         scene.setRoot(page);
         stage.setScene(scene);
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) throws Exception{
-        launch();
     }
 }
