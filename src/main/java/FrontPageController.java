d7231f925492cab937cc9e4d472b6ac20d550589
aguste20 <aguste20@student.aau.dk>
2021-04-28 08:37:40 +0200

    Documentation for switchScene methods in FrontPageController + cleanup

--- src/main/java/FrontPageController.java
+++ src/main/java/FrontPageController.java
@@ -10,10 +10,12 @@ import java.io.IOException;
 
 public class FrontPageController {
 
+    // TODO - kan også slettes
     private Stage stage;
     private Scene scene;
     private Parent root;
 
+    //TODO - kan slettes
     //Functions to switch between scenes
     public void switchToScene(ActionEvent event, Parent page) throws IOException {
         root = page;
@@ -23,10 +25,20 @@ public class FrontPageController {
         stage.show();
     }
 
+    /**
+     * Event handler for the button "Tilføj varer til beregning".
+     * Switches to the data insertion page.
+     * @param event action event from the button element
+     */
     public void switchToSceneDataInsertion(ActionEvent event){
         App.switchScene(App.getDataInsertionPageParent());
     }
 
+    /**
+     * Event handler for the button "Historik".
+     * Switches to the history page.
+     * @param event2 action event from the button element
+     */
     public void switchToSceneHistory(ActionEvent event2){
         App.switchScene(App.getHistoryPageParent());
     }
