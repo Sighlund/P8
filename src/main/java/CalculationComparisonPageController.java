cd1cb018e1c921b45cceff3b64db5c14d273a209
aguste20 <aguste20@student.aau.dk>
2021-05-04 10:24:44 +0200

    Work in progress - build stacked bar chart Comparison

--- src/main/java/CalculationComparisonPageController.java
+++ src/main/java/CalculationComparisonPageController.java
@@ -29,89 +29,51 @@ public class CalculationComparisonPageController implements Initializable {
         //calls methods to build the stacked bar chart
     }
 
-    //Categories used for stacking the bar chart
-    List<String> arr = ConcitoPersistence.getDistinctCategories();
-    String Category1 = arr.get(0);
-    String Category2 = arr.get(1);
-    String Category3 = arr.get(2);
-    String Category4 = arr.get(3);
-    String Category5 = arr.get(4);
-    String Category6 = arr.get(5);
-    String Category7 = arr.get(6);
-    String Category8 = arr.get(7);
-    String Category9 = arr.get(8);
-    String Category10 = arr.get(9);
-    String Category11 = arr.get(10);
-    String Category12 = arr.get(11);
-    String Category13 = arr.get(12);
+    private List<String> categories = new ArrayList<>();
+    private List<CalculationModel> calcs = new ArrayList<>();
+
+    // TODO Skal slettes
+    private CalculationModel calc;
 
     //Building StackedBarChart
     public void buildStackedBarChart(String... args){
 
-        XYChart.Series<String, Number> Category1 = new XYChart.Series<>();
-        Category1.setName(arr.get(0));
-        XYChart.Series<String, Number> Category2 = new XYChart.Series<>();
-        Category2.setName(arr.get(1));
-        XYChart.Series<String, Number> Category3 = new XYChart.Series<>();
-        Category3.setName(arr.get(2));
-        XYChart.Series<String, Number> Category4 = new XYChart.Series<>();
-        Category4.setName(arr.get(3));
-        XYChart.Series<String, Number> Category5 = new XYChart.Series<>();
-        Category5.setName(arr.get(4));
-        XYChart.Series<String, Number> Category6 = new XYChart.Series<>();
-        Category6.setName(arr.get(5));
-        XYChart.Series<String, Number> Category7 = new XYChart.Series<>();
-        Category7.setName(arr.get(6));
-        XYChart.Series<String, Number> Category8 = new XYChart.Series<>();
-        Category8.setName(arr.get(7));
-        XYChart.Series<String, Number> Category9 = new XYChart.Series<>();
-        Category9.setName(arr.get(8));
-        XYChart.Series<String, Number> Category10 = new XYChart.Series<>();
-        Category10.setName(arr.get(9));
-        XYChart.Series<String, Number> Category11 = new XYChart.Series<>();
-        Category11.setName(arr.get(10));
-        XYChart.Series<String, Number> Category12 = new XYChart.Series<>();
-        Category12.setName(arr.get(11));
-        XYChart.Series<String, Number> Category13 = new XYChart.Series<>();
-        Category13.setName(arr.get(12));
+        MyStackedBarChart.getData().clear();
+        categories.clear();
 
-        for(String arg: args) {
-            Category1.getData().add(new XYChart.Data<>(arg, 7));
-            Category2.getData().add(new XYChart.Data<>(arg, 9));
-            Category3.getData().add(new XYChart.Data<>(arg, 14));
-            Category4.getData().add(new XYChart.Data<>(arg, 7));
-            Category5.getData().add(new XYChart.Data<>(arg, 9));
-            Category6.getData().add(new XYChart.Data<>(arg, 8));
-            Category7.getData().add(new XYChart.Data<>(arg, 13));
-            Category8.getData().add(new XYChart.Data<>(arg, 8));
-            Category9.getData().add(new XYChart.Data<>(arg, 8));
-            Category10.getData().add(new XYChart.Data<>(arg, 5));
-            Category11.getData().add(new XYChart.Data<>(arg, 6));
-            Category12.getData().add(new XYChart.Data<>(arg, 10));
-            Category13.getData().add(new XYChart.Data<>(arg, 1));
+        Hashtable<String, Double> dict = calc.getCategoriesPercentagesDict();
 
-          }
-        MyStackedBarChart.setCategoryGap(200);
-        MyStackedBarChart.getData().addAll(Category1, Category2, Category3, Category4, Category5, Category6, Category7, Category8, Category9, Category10, Category11, Category12, Category13);
+        Set<String> keys = dict.keySet();
 
+        // Skal ændres! lige nu kun baseret på én calculation
+        for (String k : keys){
+            categories.add(k);
+        }
+
+        // Laver en serie pr. kategori
+        for (String c : categories) {
+            XYChart.Series<String, Number> series = new XYChart.Series<String, Number>();
+            series.setName(c);
+
+            // Hvis kategorien er repræsenteret i calculation, lav en data entry
+            if (keys.contains(c)) {
+                series.getData().add(new XYChart.Data<>(calc.getKitchen().toString(), dict.get(c)));
+            }
+
+            MyStackedBarChart.getData().add(series);
+
+
+            MyStackedBarChart.setCategoryGap(200);
+        }
     }
 
-    String enhed;
-    String enhed2 = "enhed2";
-
-    CalculationModel calc;
     //Receives information from HistoryPageController
     public void getInformation(CalculationModel calcid) {
         this.calc=calcid;
-        MyStackedBarChart.getData().clear();
+        //MyStackedBarChart.getData().clear();
         System.out.println(calcid.getKitchen());
-        enhed = calc.toString();
 
-        ArrayList<Integer> enhed1volumes = new ArrayList<Integer>();
-        enhed1volumes.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13));
-        System.out.println(enhed1volumes.get(2));
-
-        buildStackedBarChart(enhed, enhed2);
+        buildStackedBarChart();
     }
 
     /**
