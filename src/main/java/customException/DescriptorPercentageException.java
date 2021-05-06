7d78816294f3ab5d530f0cd9ed7b02436ba6aaae
Mads Andersen <madssighlund@live.dk>
2021-05-06 15:21:00 +0200

    Added comments

--- src/main/java/customException/DescriptorPercentageException.java
+++ src/main/java/customException/DescriptorPercentageException.java
@@ -1,7 +1,11 @@
 package customException;
 
+/**
+ * This exception class is supposed to be used to check if the total % of ingredients does not equal 100
+ */
 public class DescriptorPercentageException extends Exception{
 
+    //Used to write a exception message in the console
     public DescriptorPercentageException(String message) {
         super(message);
     }
--- src/main/java/customException/IngredientPercentageException.java
+++ src/main/java/customException/IngredientPercentageException.java
@@ -1,7 +1,12 @@
 package customException;
 
+/**
+ * This exception class is supposed to be used to check if the % of the ingredient added is less or equal
+ * than 0 or greater than 100
+ */
 public class IngredientPercentageException extends Exception{
 
+    //Used to write a exception message in the console
     public IngredientPercentageException(String message){
         super(message);
     }
